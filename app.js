cebfd1c12e5375feecec3b83d97f2cd4ab6660cc
luisarnez1986 <luisarnez@gmail.com>
2026-07-02 12:52:57 -0400

    Primera versión funcional WebAR

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("WebAR-Mobile iniciado");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const video = document.querySelector("#video");
+    const target = document.querySelector("#target");
+
+    video.load();
+
+    target.addEventListener("targetFound", () => {
+
+        console.log("TARGET ENCONTRADO");
+
+        video.currentTime = 0;
+
+        const p = video.play();
+
+        if (p !== undefined) {
+            p.then(() => {
+                console.log("VIDEO REPRODUCIÉNDOSE");
+            }).catch(err => {
+                console.error("ERROR VIDEO:", err);
+            });
+        }
+
+    });
+
+    target.addEventListener("targetLost", () => {
+
+        console.log("TARGET PERDIDO");
+
+        video.pause();
+
+    });
+
+});
